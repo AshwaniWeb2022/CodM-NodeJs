e418cc26e2d15cc626acf0b4eb7b0cd75706bcd0
Ashwani <ashwani.kumar@codmsoftware.com>
2024-04-11 14:56:46 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const app = express();
+
+const routes = require('./routes/main');
+
+app.use('/', routes);
+
+// static/css/image/
+app.use('/static', express.static("public"));
+
+// view engine
+app.set('view engine', 'hbs');
+app.set("views", "views");
+
+// DB connect
+mongoose.connect("mongodb://localhost/website_tut", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("DB connected");
+        app.listen(process.env.PORT || 8000, () => {
+            console.log("Server started");
+        });
+    })
+    .catch(err => console.error("DB connection error:", err));
