a478f54f5324003d7dc9d866c8eb3cacdb10e2c3
Ashwani <ashwani.kumar@codmsoftware.com>
2024-04-15 13:15:34 +0530

    uploade file path

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const routes = require('./src/routes/main');
 app.use('/', routes);
 
 // static/css/image/
-app.use('/static', express.static("public"));
-
+// app.use('/static', express.static("public"));
+app.use(express.static(__dirname + '/public'))
 // view engine
 app.set('view engine', 'hbs');
-app.set("views", "views");
+// app.set("views", "views");
+app.set('views', path.join(__dirname, 'views'));
+
+
 
 // DB connect
 mongoose.connect("mongodb://localhost/website_tut")
